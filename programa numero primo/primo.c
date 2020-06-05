682138719c7e098992ef357c8bd9935b3b0b2498
causticroot <https://github.com/causticroot/senaidevproject.git>
2020-06-05 02:35:26 -0300

    primos1ax

---
+++ programa numero primo/primo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int number;
+int qnt = 0;
+
+
+
+int main(void)
+{
+    printf("Escreva um número: \n");       
+        scanf("%d", &number);
+        for (int i = 1; i <= number; i++)
+        {
+            if(number % i == 0)
+            {
+             qnt++;   
+            }   
+        }
+        
+        if(qnt == 2)
+        {
+            printf("O número %d é primo \n",  number);
+        } else
+         {
+            printf("O número %d não é primo \n", number);
+         }
+        
+        
+      
+   
+
+}
