b686f1b3dbfc69abb8c28b460fb10610684795d9
causticroot <https://github.com/causticroot/senaidevproject.git>
2020-06-10 13:51:30 -0300

    new

---
+++ Atividades em C  uri/Atividade Soma impares consecutivos/atividade71.c
@@ -0,0 +1,37 @@
+/*
+Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números impares entre eles.
+
+Entrada
+O arquivo de entrada contém dois valores inteiros.
+
+Saída
+O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares que estão entre os valores 
+fornecidos na entrada que deverá caber em um inteiro.
+
+Exemplo de Entrada	Exemplo de Saída
+6
+-5
+*/
+
+#include<stdio.h>
+    int main()
+    {
+        int x, y;
+        int soma = 0;
+        int impar;
+        int p=0;
+        
+        scanf("%d %d", &x, &y);
+        while (p<y)
+        {
+            if (x)
+            {
+                /* code */
+            }
+            
+        }
+        
+
+
+
+    }
