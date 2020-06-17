aa24c739925709cffb667c45ff03e8d07dd999e4
causticroot <https://github.com/causticroot/senaidevproject.git>
2020-06-17 15:56:41 -0300

    new update

--- Atividades em C  uri/Atividade sequencia 4/atividade98.c
+++ Atividades em C  uri/Atividade sequencia 4/atividade98.c
@@ -14,13 +14,13 @@ I=2 J=?
 #include<stdio.h>
     int main()
     {
-        double matriz[18][2];
+        double matriz[36][2];
         double i = 0;
         double j = 1;
-        int l, c;
+       int l, c;
         int count = 0;
 
-        for (l = 0; l < 18; l++)
+        for (l = 0; l < 36; l++)
         {
             if (count == 3)
             {
@@ -35,7 +35,7 @@ I=2 J=?
             count++;
         }
 
-        for (l = 0; l < 18; l++)
+        for (l = 0; l < 36; l++)
         {
             printf("I=%.1lf J=%.1lf\n", matriz[l][0], matriz[l][1]);
         }
