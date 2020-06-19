91465ed0e3ae03bd9268293e47848327fca3f310
causticroot <https://github.com/causticroot/senaidevproject.git>
2020-06-19 15:45:37 -0300

    organize

--- Atividades em C  uri/Atividade mutiplos/atividade44.c
+++ Atividades em C  uri/Atividade mutiplos/atividade44.c
@@ -4,19 +4,5 @@ int count;
 int main() {
     int i;
     scanf("%d%d", &valor[0], &valor[1]);
-    for(i= 0; i <= valor[1]; i++)
-    {
-        if(count == valor[1])
-        {
-            printf("Nao sao Multiplos\n");
-        }
-        if(i * valor[0] == valor[1])
-        {
-            printf("Sao Multiplos\n");
-        }else
-        {
-            count++;
-        }
-    }
+    
     return 0;
-}
