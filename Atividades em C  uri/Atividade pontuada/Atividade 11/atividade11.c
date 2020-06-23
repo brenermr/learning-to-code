bd1f221c5c621c432b28299a899bcb61f527326e
causticroot <https://github.com/causticroot/senaidevproject.git>
2020-06-23 13:07:26 -0300

    ponteiros

--- Atividades em C  uri/Atividade pontuada/Atividade 11/atividade11.c
+++ Atividades em C  uri/Atividade pontuada/Atividade 11/atividade11.c
@@ -6,15 +6,25 @@
     int main()
     {   
         int matriz[2][8];
+        int *ponteiro;
         int i, j;
         for (i = 0; i < 2; i++)
         {
-            for (j = 0; i < 8; j++)
+            for (j = 0; j < 8; j++)
             {
-                scanf("%d",matriz[i][j]);
+                scanf("%d", &matriz[i][j]);
+            }
+        }
+        for (i = 0; i < 2; i++)
+        {
+            for (j = 0; j < 8; j++)
+            {
+                ponteiro = &matriz[i][j];
+                printf("Var value= %d\n Memory address= %p\n", matriz[i][j], ponteiro);
             }
             
         }
+        
         
         return 0;
     }
